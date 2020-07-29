be916a2f135094e054cf870db387f0342a18348f
Pavlo Kachurovskiy <kachurovskiy.p@gmail.com>
2020-07-29 09:30:52 +0200

    3. Личный проект: начинаем программировать

---
+++ src/main.js
@@ -0,0 +1,175 @@
+'use strict';
+
+const EVENTS_COUNT = 3;
+
+const pageHeaderElement = document.querySelector(`.page-header`);
+const headerMainInfoContainer = pageHeaderElement.querySelector(`.trip-main`);
+const tripControlsContainer = pageHeaderElement.querySelector(`.trip-main__trip-controls`);
+const tripViewControlsTitles = tripControlsContainer.querySelectorAll(`h2`);
+
+const tripMainElement = document.querySelector(`.page-body__page-main`);
+const tripEventsContainer = tripMainElement.querySelector(`.trip-events`);
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const createMainInfoTamplate = () => {
+  return (`
+    <section class="trip-main__trip-info  trip-info">
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+      </div>
+
+      <p class="trip-info__cost">
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+      </p>
+    </section>
+  `);
+};
+
+const createTripViewControls = () => {
+  return (`
+    <nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>
+  `);
+};
+
+const createTripFilterControls = () => {
+  return (`
+    <form class="trip-filters" action="#" method="get">
+      <div class="trip-filters__filter">
+        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+        <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      </div>
+
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>
+  `);
+};
+
+const createTripEventsSortControls = () => {
+  return (`
+    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+      <span class="trip-sort__item  trip-sort__item--day">Day</span>
+
+      <div class="trip-sort__item  trip-sort__item--event">
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
+        <label class="trip-sort__btn" for="sort-event">Event</label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <label class="trip-sort__btn" for="sort-time">
+          Time
+          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+          </svg>
+        </label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <label class="trip-sort__btn" for="sort-price">
+          Price
+          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+          </svg>
+        </label>
+      </div>
+
+      <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+    </form>
+  `);
+};
+
+const createTripDaysContainer = () => {
+  return (`
+    <ul class="trip-days">
+    </ul>
+  `);
+};
+
+const createTripDayItem = () => {
+  return (`
+    <li class="trip-days__item  day">
+      <div class="day__info">
+        <span class="day__counter">1</span>
+        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+      </div>
+
+      <ul class="trip-events__list">
+      </ul>
+    </li>
+  `);
+};
+
+const creatrTripDayEvent = () => {
+  return (`
+    <li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">Taxi to Amsterdam</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          </p>
+          <p class="event__duration">30M</p>
+        </div>
+
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">20</span>
+        </p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          <li class="event__offer">
+            <span class="event__offer-title">Order Uber</span>
+            &plus;
+            &euro;&nbsp;<span class="event__offer-price">20</span>
+          </li>
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>
+  `);
+};
+
+render(headerMainInfoContainer, createMainInfoTamplate(), `afterbegin`);
+render(tripViewControlsTitles[0], createTripViewControls(), `afterend`);
+render(tripViewControlsTitles[1], createTripFilterControls(), `afterend`);
+
+render(tripEventsContainer, createTripEventsSortControls(), `beforeend`);
+render(tripEventsContainer, createTripDaysContainer(), `beforeend`);
+
+const tripDaysContainer = tripEventsContainer.querySelector(`.trip-days`);
+
+render(tripDaysContainer, createTripDayItem(), `beforeend`);
+
+const tripEventsList = tripDaysContainer.querySelector(`.trip-events__list`);
+
+for (let i = 0; i < EVENTS_COUNT; i++) {
+  render(tripEventsList, creatrTripDayEvent(), `beforeend`);
+}
