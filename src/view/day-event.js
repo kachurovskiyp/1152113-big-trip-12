6429ec3917f94493bdca59eab31de7226a2808d8
Pawel <kachurovskiy.p@gmail.com>
2020-08-08 17:57:13 +0200

    Исправляет именование иконок

--- src/view/day-event.js
+++ src/view/day-event.js
@@ -36,7 +36,7 @@ export const createTripDayEvent = (tripPoint) => {
     <li class="trip-events__item">
       <div class="event">
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/${type === `Check` ? `Check-in` : type}.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase() === `check` ? `check-in` : type.toLowerCase()}.png" alt="Event type icon">
         </div>
         <h3 class="event__title">${type} to ${target}</h3>
 
